a1ca45bab0ce7c3c67c4af7342e3e2dd46ae2e88
Manan Mongia <mananmongia1997@gmail.com>
2025-01-16 00:17:46 +0530

    404 Updated

--- error.php
+++ error.php
@@ -1,34 +1,6 @@
 ﻿<!DOCTYPE html>
 <html lang="en">
-<head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="keywords" content="Jagdamba Minechem, Minerals, Chemicals, Manufacturing, Trading">
-<meta name="description" content="Jagdamba Minechem - Manufacturing & Trading">
-<meta name="keywords" content="Jagdamba Minechem, Minerals, Chemicals, Manufacturing, Trading">
-<meta name="author" content="https://jagdambaminechem.in/">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Jagdamba Minechem - Manufacturing & Trading</title>
-
-<link rel="shortcut icon" href="images/favicon.png">
-<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-<link rel="stylesheet" type="text/css" href="css/animate.css">
-<link rel="stylesheet" type="text/css" href="css/all.min.css">
-<link rel="stylesheet" type="text/css" href="css/fontello.css">
-<link rel="stylesheet" type="text/css" href="css/flaticon.css">
-<link rel="stylesheet" type="text/css" href="css/themify-icons.css">
-<link rel="stylesheet" type="text/css" href="css/font-awesome.css">
-<link rel="stylesheet" type="text/css" href="css/aos.css">
-<link rel="stylesheet" type="text/css" href="css/slick.css">
-<link rel="stylesheet" type="text/css" href="css/prettyPhoto.css">
-<link rel="stylesheet" type="text/css" href="css/shortcodes.css">
-<link rel="stylesheet" type="text/css" href="css/main.css">
-<link rel="stylesheet" type="text/css" href="css/megamenu.css">
-<link rel="stylesheet" type="text/css" href="css/responsive.css">
-
-<!-- REVOLUTION LAYERS STYLES -->
-<link rel='stylesheet' id='rs-plugin-settings-css' href="revolution/css/rs6.css">
-</head>
+<?php include "head.php" ?>
 <body>
     <!-- page start -->
     <div class="page">
@@ -58,37 +30,14 @@
         </div><!-- site-main end-->
 
         <!-- back-to-top start -->
-        <a id="totop" href="#top">
-            <i class="icon-angle-up"></i>
-        </a>
+        <?php include "totop.php" ?>
         <!-- back-to-top end -->
 
     </div><!-- page end -->
 
 
     <!-- Javascript -->
-    <script src="js/jquery-3.7.1.min.js"></script>
-<script src="js/jquery-migrate-3.4.1.min.js"></script>
-    <script src="js/bootstrap.bundle.js"></script> 
-    <script src="js/jquery-validate.js"></script> 
-    <script src="js/jquery.prettyPhoto.js"></script>
-    <script src="js/slick.min.js"></script>
-    <script src="js/jquery-waypoints.js"></script>    
-    <script src="js/numinate.min.js"></script>
-    <script src="js/cursor.min.js"></script>
-    <script src="js/gsap.js"></script>
-    <script src="js/splittext.min.js"></script>
-    <script src="js/scrolltrigger.js"></script>
-    <script src="js/imagesloaded.min.js"></script>
-    <script src="js/circle-progress.min.js"></script>
-    <script src="js/main.js"></script>
-    <script src="js/aos.js"></script>
-      <script>
-        AOS.init({
-            offset: 120,
-            duration: 400,
-        });
-      </script>
+    <?php include "script.php" ?>
     <!-- Javascript end-->
 
 </body>
